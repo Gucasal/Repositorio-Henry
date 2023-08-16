7995785a8ebadae5faf561a6c53942085dc9ecc6
Gucasal <guillermocasallobos10@gmail.com>
2023-08-16 18:36:32 -0300

    Operaciones

---
+++ Operadores.js
@@ -0,0 +1,19 @@
+console.log(3 + 5);
+console.log(14 - 7);
+console.log(4 * 6);
+console.log(30 / 9);
+console.log(21 % 5);
+console.log(3 + 5 * 2 - 8);
+console.log((3 + 5) * 2 - 8);
+
+//Adición con Strings
+
+console.log("HOLA" + "GUILLE");
+console.log("HOLA " + "GUILLE");
+
+//Crear variables más adición
+var nombre = "GUI";
+var salut = "HOLA " + nombre;
+console.log(salut);
+
+
