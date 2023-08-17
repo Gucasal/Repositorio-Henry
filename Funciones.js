5a67ae962f016edc35777b8aa5cde2daebdde63e
Gucasal <guillermocasallobos10@gmail.com>
2023-08-17 15:59:51 -0300

    Funciones

---
+++ Funciones.js
@@ -0,0 +1,22 @@
+function sumaTres(x) {
+    console.log(x + 3);
+}
+
+sumaTres(5)
+
+//Función: sumaTres
+var sumaTres = (x) => {
+    return x + 3;
+}
+
+//Función: cuidadoConElConsoleLog
+function cuidadoConElConsoleLog(nombre) {
+    console.log(nombre);
+    return nombre;
+}
+
+//Precedencia de valores
+var a = 1;
+var b = 2;
+var c = a = b;
+console.log (c);
